1f29e031fbcf02d13be6202d719e79f7a4f82fb0
deep3576 <deep3576@gmail.com>
2026-02-25 09:50:59 -0500

    updating Adding google Calender Events and user_edit update

--- db/releases/google_meet_link.sql
+++ db/releases/google_meet_link.sql
@@ -1,4 +1,2 @@
-USE `deep3576$TheSpiritSchool_ProdDB`;
-
 ALTER TABLE `booking`
-  ADD COLUMN IF NOT EXISTS `google_meet_link` VARCHAR(500) NULL AFTER `status`;
+  ADD COLUMN  `google_meet_link` VARCHAR(500) NULL AFTER `status`;
