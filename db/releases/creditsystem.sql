b410a6341a1c2ed2b3f065fdfa70a0d312540e73
deep3576 <deep3576@gmail.com>
2026-02-24 21:35:07 -0500

    Adding credit system.

--- db/releases/creditsystem.sql
+++ db/releases/creditsystem.sql
@@ -3,6 +3,7 @@ CREATE TABLE transactions (
     id INT NOT NULL AUTO_INCREMENT,
     teacher_id INT NOT NULL DEFAULT 0,
     type_of_transaction ENUM('credit', 'debit') NOT NULL,
+    mode_of_payment ENUM('cash','online') ,
     student_id INT NOT NULL,
     action_performer_user_id INT NOT NULL,
     balance_before_this_transaction INT,
