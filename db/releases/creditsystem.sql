ebce666cdd2835fce5523a6561e9706f777fc855
deep3576 <deep3576@gmail.com>
2026-02-24 21:45:55 -0500

    Adding credit system.

--- db/releases/creditsystem.sql
+++ db/releases/creditsystem.sql
@@ -1,6 +1,6 @@
 Alter table `user` Add column available_credits int Not NULL DEFAULT 0 After assigned_class_id
 CREATE TABLE transactions (
-    id INT NOT NULL AUTO_INCREMENT,
+    transaction_id INT NOT NULL AUTO_INCREMENT,
     teacher_id INT NOT NULL DEFAULT 0,
     type_of_transaction ENUM('credit', 'debit') NOT NULL,
     mode_of_payment ENUM('cash','online') ,
@@ -12,5 +12,11 @@ CREATE TABLE transactions (
     credits_added INT,
     total_available_credits INT,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Good practice for transaction logs
-    PRIMARY KEY (id)
+    PRIMARY KEY (transaction_id)
 );
+ALTER TABLE booking ADD CONSTRAINT fk_booking_transaction
+FOREIGN KEY (id) REFERENCES transactions(transaction_id);
+
+
+
+
